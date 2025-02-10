0841b19bdf30d2d2d5f2a5222a4b99cd7a41c306
HesamSe <hesam.seyfollahi@gmail.com>
2025-02-10 11:40:58 +0330

    add projects

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -10,6 +10,18 @@ export const projects: Project[] = [
     title: "Word Lens",
     year: 2025,
     description: "Learn the words of your book!",
-    url: "https://word-lens.vercel.app",
+    url: "https://github.com/HesamSe/word-lens",
+  },
+  {
+    title: "eslint-plugin-recommended",
+    year: 2025,
+    description: "My custom eslint rules",
+    url: "https://github.com/HesamSe/eslint-plugin-recommended",
+  },
+  {
+    title: "Top 150 interviews",
+    year: 2025,
+    description: "Leetcode.com top interview questions",
+    url: "https://github.com/HesamSe/top-150-interview",
   },
 ];
